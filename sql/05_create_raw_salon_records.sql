9be62e78325e47ff3fc22657b62f2d2d54f07e99
Ann-Kiende <annkiende30@gmail.com>
2026-06-05 23:07:49 +0300

    Add staging table for raw salon records import

---
+++ sql/05_create_raw_salon_records.sql
@@ -0,0 +1,10 @@
+CREATE TABLE RawSalonRecords (
+    AppointmentDate DATE,
+    NailTech VARCHAR(100),
+    ClientName VARCHAR(100),
+    PhoneNumber VARCHAR(20),
+    ServiceName VARCHAR(100),
+    Amount INT,
+    Tip INT,
+    PaymentMode VARCHAR(50)
+)
