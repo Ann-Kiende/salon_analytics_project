d712468f7ba71494ae376e46e44a1e8e098e7a2d
Ann-Kiende <annkiende30@gmail.com>
2026-07-13 18:18:56 +0300

    Add test_environment.sql file that I will be using to test queries before I commit them to GitHub

---
+++ sql/test_environment.sql
@@ -0,0 +1,9 @@
+SELECT * FROM AppointmentServices
+SELECT * FROM RawSalonRecords
+
+SELECT * FROM Clients
+SELECT * FROM PaymentModes
+
+SELECT * FROM Services
+SELECT * FROM Appointments
+SELECT * FROM NailTechs
