642f28525b059203a691292c83aeb6ffd7d958ee
Ann-Kiende <annkiende30@gmail.com>
2026-07-14 23:59:39 +0300

    Test codes

--- sql/test_environment.sql
+++ sql/test_environment.sql
@@ -7,3 +7,15 @@ SELECT * FROM PaymentModes
 SELECT * FROM Services
 SELECT * FROM Appointments
 SELECT * FROM NailTechs
+
+-- Compare revenue by service - Normalize tables
+SELECT
+    rsr.AppointmentDate,
+    c.ClientID,
+    rsr.Tip,
+    pm.PaymentModeID
+FROM RawSalonRecords rsr
+JOIN Clients c
+    ON rsr.PhoneNumber = c.PhoneNumber
+JOIN PaymentModes pm
+    ON rsr.PaymentMode = pm.PaymentModeName
